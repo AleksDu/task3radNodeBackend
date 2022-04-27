938525eda707c29f5fb6c3d729f8bc7af9020347
AleksDu <aleksandr.dyshkin@gmail.com>
2022-04-27 17:23:24 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+## GoIT Node.js Course Template Homework
+Radency HW3 Node BackEbnd
+
+- JS-код чистий і зрозумілий, для форматування викор Prettier
+
+### Команды:
+
+- `npm start` &mdash; старт сервера в режимі production
+- `npm run start:dev` &mdash; старт сервера в режиміе разробки (development)
+- `npm run lint` &mdash; запустить виконання перевірки кода с eslint, необхідно виконувати перед кожним PR и виправлять всі помилки лінтера
+- `npm lint:fix` &mdash; така ж перевірка лінтера, але з автоматичними виправленями простих помилок
---
+++ routes/api/notes/validation.js
@@ -0,0 +1,88 @@
+import Joi from "joi";
+import pkg from "mongoose";
+
+const { Types } = pkg;
+
+const createSchema = Joi.object({
+  text: Joi.string().min(2).max(30).required(),
+  category: Joi.string().required(),
+  time: Joi.string().required(),
+});
+
+const updateSchema = Joi.object({
+  text: Joi.string().optional(),
+  category: Joi.string().email().optional(),
+  isArchived: Joi.boolean().optional(),
+}).or("text", "category", "isArchived");
+
+// const regLimit = /\d+/;
+
+// const querySchema = Joi.object({
+//   limit: Joi.string().pattern(regLimit).optional(),
+//   skip: Joi.number().min(0).optional(),
+//   sortBy: Joi.string().valid("name", "email").optional(),
+//   sortByDesc: Joi.string().valid("name", "email").optional(),
+//   filter: Joi.string()
+//     // eslint-disable-next-line prefer-regex-literals
+//     .pattern(new RegExp("(name|email)\\|?(name|email)+"))
+//     .optional(),
+// });
+
+const IdSchema = Joi.object({
+  id: Joi.string().required(),
+});
+
+export const validateCreate = async (req, res, next) => {
+  try {
+    await createSchema.validateAsync(req.body);
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ message: `Field ${err.message.replace(/"/g, "")}` });
+  }
+  next();
+};
+
+export const validateUpdate = async (req, res, next) => {
+  try {
+    await updateSchema.validateAsync(req.body);
+  } catch (err) {
+    const [{ type }] = err.details;
+    if (type === "object.missing") {
+      return res.status(400).json({ message: "missing fields" });
+    }
+    return res.status(400).json({ message: err.message });
+  }
+  next();
+};
+
+// export const validateUpdateFavorite = async (req, res, next) => {
+//   try {
+//     await updateFavoriteSchema.validateAsync(req.body);
+//   } catch (err) {
+//     const [{ type }] = err.details;
+//     if (type === "object.missing") {
+//       return res.status(400).json({ message: "missing field favorite" });
+//     }
+//     return res.status(400).json({ message: err.message });
+//   }
+//   next();
+// };
+
+export const validateId = async (req, res, next) => {
+  if (!Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ message: "Invalid ObjectId" });
+  }
+  next();
+};
+
+// export const validateQuery = async (req, res, next) => {
+//   try {
+//     await querySchema.validateAsync(req.query);
+//   } catch (err) {
+//     return res
+//       .status(400)
+//       .json({ message: `Field ${err.message.replace(/"/g, "")}` });
+//   }
+//   next();
+// };
